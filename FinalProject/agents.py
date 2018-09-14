c4be7a84949bc73cfda405f79ba91ced0fc2c401
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-09-14 00:12:00 -0300

    implemented online training and fixed FPS

--- FinalProject/agents.py
+++ FinalProject/agents.py
@@ -1,24 +1,37 @@
-from keras.models import Sequential
+from keras.models import Sequential, clone_model
 from keras.layers import Dense
 import numpy as np
+from threading import Thread
 
 class MultilayerPerceptron():
     def __init__(self, obsSpace):
-        nPixels = obsSpace.sample().size
-        self.model = Sequential()
-        self.model.add(Dense(units=400, activation='sigmoid', kernel_initializer="uniform", input_dim=nPixels))
-        self.model.add(Dense(units=20, activation='sigmoid', kernel_initializer="uniform"))
-        self.model.add(Dense(units=5, activation='sigmoid', kernel_initializer="uniform"))
-        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        self.nPixels = obsSpace.sample().size
+        self.trainModel = Sequential()
+        self.trainModel.add(Dense(units=40, activation='sigmoid', kernel_initializer="uniform", input_dim=self.nPixels))
+        self.trainModel.add(Dense(units=20, activation='sigmoid', kernel_initializer="uniform"))
+        self.trainModel.add(Dense(units=5, activation='sigmoid', kernel_initializer="uniform"))
+        self.trainModel.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+        self.agentModel = clone_model(self.trainModel)
+        self.currBatchSize = 0
+        self.observationBatch = np.empty((0, self.nPixels))
+        self.actionBatch = np.empty((0, 5))
+        self.trainThread = None
 
     def train(self, observation, action):
-        flatScr = observation.flatten()
-        print(flatScr.shape)
-        self.model.train_on_batch(flatScr, action)
+        flatScr = np.expand_dims(observation.flatten(), 0)
+        action = np.expand_dims(action, 0)
+        self.observationBatch = np.append(self.observationBatch, flatScr, 0)
+        self.actionBatch = np.append(self.actionBatch, action, 0)
+        self.currBatchSize += 1
+        if self.currBatchSize > 5:
+            observationBatch = np.copy(self.observationBatch)
+            actionBatch = np.copy(self.actionBatch)
+            self.currBatchSize = 0
+            self.observationBatch = np.empty((0, self.nPixels))    
+            self.actionBatch = np.empty((0, 5))
+            self.trainModel.train_on_batch(observationBatch, actionBatch)
 
     def action(self, observation):
-        flatScr = observation.flatten()
-        print(flatScr.shape)
-        output = self.model.predict(flatScr)
-        return output
-
+        flatScr = np.expand_dims(observation.flatten(), 0)
+        output = self.agentModel.predict(flatScr)
+        return np.greater(output, 0.5).astype(np.float64)[0]
--- FinalProject/autoplay.py
+++ FinalProject/autoplay.py
@@ -6,9 +6,10 @@ import pynput.keyboard as kb
 from joypad import Joypad
 from games import gameList
 from agents import MultilayerPerceptron
+import numpy as np
 
-FPS = 30.0
-framePeriod = 1.0/30.0
+FPS = 20.0
+framePeriod = 1.0/FPS
 
 joypad = Joypad()
 
@@ -16,7 +17,7 @@ def main():
     kbListener = kb.Listener(on_press=joypad.on_press, on_release=joypad.on_release)
     kbListener.start()
 
-    env = gym.make(gameList[1])
+    env = gym.make(gameList[0])
     agent = MultilayerPerceptron(env.observation_space)
     
     env.reset()
@@ -28,13 +29,14 @@ def main():
             action = joypad.action
             if action == -1:
                 break
-            if time.time() - lastRender > framePeriod:
+            currPeriod = time.time() - lastRender
+            if currPeriod > framePeriod:
                 lastRender = time.time()
                 observation, _, done, _ = env.step(action)
                 agent.train(observation, joypad.actionButtons())
-                print('{} {} Autoplay:{}'.format(joypad.actionButtons(),
-                                                 joypad.lastKey,
-                                                 joypad.autoplay))
+                print('{} Autoplay:{}, FPS:{}'.format(joypad.actionButtons(),
+                                                 joypad.autoplay,
+                                                 np.round(1/currPeriod)))
                 print(agent.action(observation))
                 if done:
                     env.reset()
