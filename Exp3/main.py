102c846d08ac3732dbbfd7cfd386a5c2181d2aa2
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-10-14 15:33:56 -0300

    Add save/load Keras model functionality

--- Exp3/main.py
+++ Exp3/main.py
@@ -2,10 +2,11 @@
 
 import numpy as np
 import time
+import os
 
 from keras.datasets import cifar100
 
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 
@@ -13,6 +14,9 @@ from keras.callbacks import EarlyStopping
 from keras.utils import to_categorical
 
 def main():
+    modelName = 'model1'
+    modelFilename = '{}.h5'.format(modelName)
+    
     np.random.seed(int(time.time()))
 
     (x_train, y_train_idx), (x_test, y_test_idx) = cifar100.load_data(label_mode='fine')
@@ -21,32 +25,39 @@ def main():
     
     num_classes = y_test[0].size
 
-    print(x_train[0].shape)
+    if os.path.isfile(modelFilename):
+        print('Loading pre-trained model')
+        model = load_model(modelFilename)
+    else:
+        print('Creating model')
+        model = Sequential()
 
-    model = Sequential()
+        model.add(Conv2D(64, (3, 3), input_shape=x_train[0].shape, padding='same', activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3), input_shape=x_train[0].shape, padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(2048, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_classes, activation='softmax'))
 
-    model.add(Flatten())
-    model.add(Dense(2048, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.save(modelFilename)
 
     earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 
     model.fit(x_train, y_train, validation_data=(x_test, y_test), 
-          epochs=1, batch_size=50, shuffle=True, callbacks=[earlyStopping])
+          epochs=4, batch_size=50, shuffle=True, callbacks=[earlyStopping])
+
+    model.save(modelFilename)
 
 if __name__ == '__main__':
     main()
