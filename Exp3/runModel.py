5a4575641a367c04d2f47db26c04ac8738c7adde
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-10-21 18:24:53 -0300

    Improve runModel.py

--- Exp3/runModel.py
+++ Exp3/runModel.py
@@ -4,6 +4,8 @@ import numpy as np
 import time
 import os
 import sys
+import shutil
+
 from keras.datasets import cifar10
 
 from keras.models import Sequential, load_model
@@ -17,26 +19,32 @@ def main():
         print("Usage: './main.py <modelname> <number_of_samples_to_test>")
         exit()
     modelName = sys.argv[1]
+    nSamples = int(sys.argv[2])
     modelFilename = '{}.h5'.format(modelName)
 
     print('Loading pre-trained model {}'.format(modelName))
     model = load_model(modelFilename)
-
-    if os.path.isdir(modelName):
-        os.rmdir(modelName)
-    os.makedirs(os.path.join(modelName, exist_ok=True))
     print(model.summary())
 
-    _, (x_test, y_test) = cifar10.load_data()
-    choices = np.random.choice(x_test.shape[0], 3, replace=False)
-    x_test = x_test[choices]
-    y_test = to_categorical(y_test[choices])
+    if os.path.isdir(modelName):
+        shutil.rmtree(modelName)
 
-    for idx, img, target in enumerate(zip(x_test, y_test)):
-        model.predict_proba(img)
-        guess = model.predict(img)
-        if guess == target:
-            print('Hit! {}'.format(idx))
+    os.makedirs(os.path.join(modelName, 'hit'), exist_ok=True)
+    os.makedirs(os.path.join(modelName, 'miss'), exist_ok=True)
+
+    _, (x_test, y_test) = cifar10.load_data()
+    choices = np.random.choice(x_test.shape[0], nSamples, replace=False)
+    x_test = x_test[choices]
+    y_test = np.argmax(to_categorical(y_test[choices]), axis=1)
+
+    proba = model.predict(x_test)
+    guess = np.argmax(proba, axis=1)
+
+    print(proba.shape, guess.shape, y_test.shape)
+
+    for idx, (g, p, target) in enumerate(zip(guess, proba, y_test)):
+        if g == target:
+            print(' Hit! {}'.format(idx))
         else:
             print('Miss! {}'.format(idx))
 
